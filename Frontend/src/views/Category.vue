74eeffa50888c985944b674c2eb2d1d6d3debe7d
Diego-Pinzon <dialpinzonm@gmail.com>
2020-12-20 22:21:16 -0500

    actualización front

---
+++ Frontend/src/views/Category.vue
@@ -0,0 +1,11 @@
+<template>
+    <category-table> </category-table>
+</template>
+
+<script>
+import CategoryTable from '../components/CategoryTable.vue'
+export default {
+  components: { CategoryTable },
+    
+}
+</script>
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <v-carousel>
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
